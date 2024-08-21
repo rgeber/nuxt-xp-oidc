552258115985ee564b0210f9e05df23db5cbf5ee
Roman Geber <dlz9zxdkjptu@romangeber.com>
2024-08-21 09:04:55 +0200

    Initial clean commit

---
+++ pages/auth/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col gap-4 items-center">
+
+    <h3>Custom login page</h3>
+
+    <button
+        class="btn-base btn-login"
+        @click="login('oidc')"
+    >
+      <span class="pl-2">Login</span>
+    </button>
+    <p>
+      Logged in:
+      <hr>
+      {{ loggedIn }}
+    </p>
+    <p>
+      <strong>User info:</strong>
+      <hr>
+      {{ user }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+const {loggedIn, user, refresh, login, logout, currentProvider, clear} = useOidcAuth()
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>OpenID connect example</h3>
+
+    <p>This example illustrates how to connect an OpenID connect service (e.g. authentik) using <a
+        href="https://nuxt.com/modules/nuxt-oidc-auth">nuxt-oidc-auth</a>.</p>
+
+    <button @click="login()">Login</button>
+    <button @click="logout('oidc')">Logout</button>
+
+    <p>
+      Logged in:
+      <hr>
+      {{ loggedIn }}
+    </p>
+    <p>
+      <strong>User info:</strong>
+      <hr>
+      {{ user }}
+    </p>
+  </div>
+
+
+</template>
+
+<script setup>
+const {loggedIn, user, refresh, login, logout, currentProvider, clear} = useOidcAuth()
+</script>
